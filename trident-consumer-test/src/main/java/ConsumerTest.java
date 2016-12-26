d6bbe94d51de726a73d784dd9a07ee8cece0e8bb
Pavel <popov.pa@yandex.ru>
2016-12-26 14:09:49 +0300

    Storm Trident

---
+++ trident-consumer-test/src/main/java/ConsumerTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Pavel Popov on 26.12.2016.
+ */
+public class ConsumerTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ trident-producer-test/src/main/java/ProducerTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Pavel Popov on 26.12.2016.
+ */
+public class ProducerTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
